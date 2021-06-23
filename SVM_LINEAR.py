5cd2aac0dc1fed8b8884bcf398849afbb58312d5
ishakarthika <ishakarthika2000@gmail.com>
2021-06-23 11:48:22 +0530

    Added source code for svm classifier

---
+++ SVM_LINEAR.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[62]:
+
+
+fruits = pd.read_csv(r"C:\Users\Isha\Documents\EDA\apple.csv")
+
+
+# In[63]:
+
+
+fruits.head()
+
+
+# In[64]:
+
+
+#Splitting the dataset into training and test samples
+from sklearn.model_selection import train_test_split
+training_set, test_set = train_test_split(fruits, test_size = 0.2, random_state = 1)
+
+
+# In[66]:
+
+
+#Classifying the predictors and target
+X_train = training_set.iloc[:,0:2].values
+Y_train = training_set.iloc[:,2].values
+X_test = test_set.iloc[:,0:2].values
+Y_test = test_set.iloc[:,2].values
+
+
+# In[67]:
+
+
+#Initializing Support Vector Machine and fitting the training data
+from sklearn.svm import SVC
+classifier = SVC(kernel='linear', random_state = 1)
+classifier.fit(X_train,Y_train)
+
+
+# In[68]:
+
+
+#Predicting the classes for test set
+Y_pred = classifier.predict(X_test)
+
+
+# In[69]:
+
+
+#Attaching the predictions to test set for comparing
+test_set["Predictions"] = Y_pred
+
+
+# In[70]:
+
+
+#Calculating the accuracy of the predictions
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test,Y_pred)
+accuracy = float(cm.diagonal().sum())/len(Y_test)
+print("\nAccuracy Of SVM For The Given Dataset : ", accuracy)
+print("\nConfusion matrix : \n", cm)
+
+
+# In[71]:
+
+
+#Visualizing the classifier
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+Y_train = le.fit_transform(Y_train)
+
+
+# In[74]:
+
+
+from sklearn.svm import SVC
+classifier = SVC(kernel='linear', random_state = 1)
+classifier.fit(X_train,Y_train)
+
+
+# In[75]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+plt.figure(figsize = (7,7))
+X_set, y_set = X_train, Y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01), np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha = 0.75, cmap = ListedColormap(('black', 'white')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'orange'))(i), label = j)
+    plt.title('Apples Vs Oranges')
+    plt.xlabel('Weight In Grams')
+    plt.ylabel('Size in cm')
+    plt.legend()
+    plt.show()
+
+
+# In[20]:
+
+
+#Visualizing the predictions
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+plt.figure(figsize = (7,7))
+X_set, y_set = X_test, Y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),alpha = 0.75, cmap = ListedColormap(('black', 'white')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],c = ListedColormap(('red', 'orange'))(i), label = j)
+    plt.title('Apples Vs Oranges Predictions')
+    plt.xlabel('Weight In Grams')
+    plt.ylabel('Size in cm')
+    plt.legend()
+    plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[23]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
